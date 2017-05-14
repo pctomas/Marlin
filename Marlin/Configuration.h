279f561fe423c0d2e2c99b564ee8917c509d3daf
Patrick Tomas <mail@patricktomas.me>
2017-05-14 01:04:12 -0700

    Heated bed PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -351,11 +351,16 @@
 
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
+  // Rigidbot 1 Heated Bed (Aluminum Plate)
+  #define  DEFAULT_bedKp 248.79
+  #define  DEFAULT_bedKi 45.19
+  #define  DEFAULT_bedKd 342.40
+
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+  //#define  DEFAULT_bedKp 10.00
+  //#define  DEFAULT_bedKi .023
+  //#define  DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
